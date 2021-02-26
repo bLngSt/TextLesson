52c1d49d3a742d573542269e0e9c75a4ecf04338
bLngSt <75664742+bLngSt@users.noreply.github.com>
2021-02-26 14:36:07 +0900

    フォントの太さの変更

--- TextLesson/ContentView.swift
+++ TextLesson/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("TEXT").font(.title).padding()
+            Text("TEXT")
+                .font(.title)
+                .fontWeight(.thin)
+                .padding()
             Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
         }
     }
