8a3b2447bfc5e7eeb47413709fd544e6d6ce0232
bLngSt <75664742+bLngSt@users.noreply.github.com>
2021-02-26 14:27:51 +0900

    Initial Commit

---
+++ TextLesson/TextLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextLessonApp.swift
+//  TextLesson
+//
+//  Created by にょにょ on 2021/02/26.
+//
+
+import SwiftUI
+
+@main
+struct TextLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
